38926661e68b0f108eef1f236863b2d2a5693eec
Jonathan Stang <jonathan.stang@inmeta.no>
2020-11-13 10:43:48 +0100

    Config changes - this app now works with confidential client takst-api in realm takst.

--- src/main/java/com/example/demo/config/KeycloakSecurityConfig.java
+++ src/main/java/com/example/demo/config/KeycloakSecurityConfig.java
@@ -28,7 +28,8 @@ public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter
             .antMatchers("/hello/**").authenticated()
             .anyRequest()
             .permitAll();
-        http.csrf().disable();
+        //http.csrf().disable()
+        ;
     }
 
     @Autowired
